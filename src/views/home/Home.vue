6a638bcdd31bd763c5ad7a2d7c7f9f28ae121fb1
Your Name <email@example.com>
2021-07-15 15:49:01 +0800

    第二次初始化项目

---
+++ src/views/home/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners='banners'/>
+    <recommend-view :recommends='recommends'/>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar';
+import HomeSwiper from './childComps/HomeSwiper';
+import RecommendView from './childComps/RecommendView';
+import {getHomeMultidata} from 'network/home';
+
+
+
+
+export default {
+  name:'Home',
+  data(){
+    return{
+    //  result: null
+    banners:[],
+    recommends:[]
+    
+
+    }
+  },
+  components:{
+    NavBar,
+    HomeSwiper,
+    RecommendView
+    
+    
+    
+  },
+  created(){
+    //1.请求多个数据
+    getHomeMultidata().then(res => {
+      
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    
+      
+  })
+  }
+  }
+
+</script>
+ 
+<style>
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
